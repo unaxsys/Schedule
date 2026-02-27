ea2fea312defe21afd17553b01f3253183e0c9e6
unaxsys <ogi.stoev80@gmail.com>
2026-02-27 18:41:02 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
+const { createHolidayService } = require('./holidayService');
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
